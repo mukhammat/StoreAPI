e850ec8afcbaddd1a47b7e82cfed9fb174dfb693
dosnet <dosnet2200@gmail.com>
2024-01-18 21:05:36 +0300

    products

---
+++ controllers/products.js
@@ -0,0 +1,64 @@
+const Product = require('../models/product');
+
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({}).sort('name').select('name price').limit(10);
+    res.status(200).json({ products, hbHits: products.length });
+}
+
+const getAllProducts = async (req, res) => {
+    const { featured, company, name, sort, fields, } = req.query;
+    const queryObject = {};
+
+    if(featured) {
+        queryObject.featured = featured === 'true' ? true : false;
+    }
+    if(company) {
+        queryObject.company = company;
+    }
+    if(name) {
+        queryObject.name = {
+            $regex: name, 
+            $options: 'i',
+        };
+    }
+
+    let result = Product.find(queryObject);
+
+    //sort
+    if(sort) {
+        console.log(sort)
+        const sortList = sort.split(',').join(' ');
+        console.log(sortList);
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');  
+    }
+
+
+    //select
+    if(fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit =  Number(req.query.limit) || 10;
+    const skip = (page -1)*limit;
+
+    result = result.skip(skip).limit(limit);
+
+
+
+
+
+
+
+    const products = await result;
+    res.status(200).json({products, hbHits: products.length});
+}
+
+
+module.exports = {
+    getAllProducts, 
+    getAllProductsStatic,
+}
